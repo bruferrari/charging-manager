142a11b2c2ca3449d1a1b248c7fe922bf13b7f6f
Bruno Ferrari <b_ferrari@live.com>
2016-01-20 02:44:34 -0200

    added register edit

--- src/main/java/com/ferrarib/charging/controller/TitleController.java
+++ src/main/java/com/ferrarib/charging/controller/TitleController.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ferrarib.charging.model.Title;
 import com.ferrarib.charging.model.TitleStatus;
@@ -20,28 +22,29 @@ import com.ferrarib.charging.repository.Titles;
 @RequestMapping("/titles")
 public class TitleController {
 	
+	private static final String REGISTER_VIEW = "TitleRegister";
+	
 	@Autowired
 	private Titles titles;
 
 	@RequestMapping("/new")
 	public ModelAndView newRegister() {
-		ModelAndView mv = new ModelAndView("TitleRegister");
+		ModelAndView mv = new ModelAndView(REGISTER_VIEW);
 		mv.addObject(new Title());
 		return mv;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView salvar(@Validated Title title, Errors errors) {
-		ModelAndView mv = new ModelAndView("TitleRegister");
+	public String salvar(@Validated Title title, Errors errors, RedirectAttributes attributes) {
 		
 		if(errors.hasErrors()) {
-			return mv;
+			return REGISTER_VIEW;
 		}
 		
 		titles.save(title);
 		
-		mv.addObject("message", title.getDescription() + " has been stored with success!");
-		return mv;
+		attributes.addFlashAttribute("message", title.getDescription() + " has been stored with success!");
+		return "redirect:/titles/new"; 
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
@@ -50,6 +53,13 @@ public class TitleController {
 		
 		ModelAndView mv = new ModelAndView("SearchTitles");
 		mv.addObject("titles", allTitles);
+		return mv;
+	}
+	
+	@RequestMapping(value="{id}", method=RequestMethod.GET)
+	public ModelAndView edit(@PathVariable("id") Title title) {
+		ModelAndView mv = new ModelAndView(REGISTER_VIEW);
+		mv.addObject("title", title);
 		return mv;
 	}
 	
