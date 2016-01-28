beadfc8dc5cb7ea62bce19714e8eb01ba850809a
Bruno Ferrari <b_ferrari@live.com>
2016-01-28 02:29:57 -0200

    added maskMoney on register titles screen

--- src/main/resources/static/js/charging.js
+++ src/main/resources/static/js/charging.js
@@ -17,4 +17,5 @@ $('#confirmRemoveModal').on('show.bs.modal', function(event) {
 
 $(function() {
 	$('[rel="tooltip"]').tooltip();
+	$('.js-currency').maskMoney({decimal: ',', thousands: '.', allowZero: true});
 });
